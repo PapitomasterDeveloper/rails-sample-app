f3f14c2b44f4c3f2587696d7aed0fbe6d2f8e55e
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2019-06-28 22:41:54 -0500

    Created a simple validation test working with setup to run on all tests cases, filling the required fields for a new User

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,13 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+	# The setup method runs automatically before each test
+	def setup
+		# Becaouse @user is an instance variable, it's available across all tests
+		@user = User.new(name: "Angel", email: "bizarroflame@gmail.com")
+	end
+
+	test "should be valid" do
+		assert @user.valid?
+	end
 end
